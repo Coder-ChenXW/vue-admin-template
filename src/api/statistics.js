9b5c9538970394be14e23c18b6e7d692e5db0478
ChenXW <chenxw1024@163.com>
2023-03-31 14:35:34 +0800

    first commit

---
+++ src/api/statistics.js
@@ -0,0 +1,13 @@
+import request from '@/utils/request'
+
+export default {
+  getCountMap(searchObj) {
+    return request({
+      url: `/admin/statistic/countByDate`,
+      method: 'get',
+      params: searchObj
+    })
+  }
+
+}
+
